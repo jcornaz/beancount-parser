dae119153dd705c9e0463bc6793b55689a4c2f7e
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-09-10 15:18:39 +0200

    feat: parse directives

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,10 @@ mod transaction;
 
 pub use account::Account;
 pub use date::Date;
+use directive::directive;
 pub use directive::Directive;
 pub use error::Error;
+use nom::{bytes::complete::take_while, sequence::preceded, IResult};
 pub use transaction::{Posting, Transaction};
 
 pub struct Parser<'a> {
@@ -22,7 +24,9 @@ pub struct Parser<'a> {
 
 impl<'a> Parser<'a> {
     pub fn new(content: &'a str) -> Self {
-        Self { rest: content }
+        Self {
+            rest: content.trim(),
+        }
     }
 }
 
@@ -30,6 +34,23 @@ impl<'a> Iterator for Parser<'a> {
     type Item = Result<(Date, Directive<'a>), Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        if self.rest.is_empty() {
+            return None;
+        }
+
+        Some(match next(self.rest) {
+            Ok((rest, directive)) => {
+                self.rest = rest;
+                Ok(directive)
+            }
+            Err(_) => {
+                self.rest = "";
+                Err(Error)
+            }
+        })
     }
+}
+
+fn next(input: &str) -> IResult<&str, (Date, Directive<'_>)> {
+    preceded(take_while(|c: char| c.is_whitespace()), directive)(input)
 }
--- tests/spec.rs
+++ tests/spec.rs
@@ -1,11 +1,24 @@
-use beancount_parser::Parser;
+use beancount_parser::{Directive, Parser};
 use rstest::rstest;
 
 const EXAMPLE1: &str = include_str!("examples/example1.beancount");
 
 #[rstest]
-fn valid_examples_should_not_return_an_error(#[values(EXAMPLE1)] input: &str) {
+fn valid_examples_should_not_return_an_error(#[values("", " \n ", EXAMPLE1)] input: &str) {
     for result in Parser::new(input) {
         assert!(result.is_ok());
     }
+}
+
+#[rstest]
+#[case("", 0)]
+#[case(EXAMPLE1, 3)]
+fn examples_have_expected_number_of_transaction(
+    #[case] input: &str,
+    #[case] expected_count: usize,
+) {
+    let actual_count = Parser::new(input)
+        .filter(|d| matches!(d, Ok((_, Directive::Transaction(_)))))
+        .count();
+    assert_eq!(actual_count, expected_count);
 }
