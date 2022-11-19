ce550c492fc68555643c894bcf5a02814150959d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-11-19 12:35:45 +0100

    test: add another error line-number test case

--- tests/unstable_spec.rs
+++ tests/unstable_spec.rs
@@ -6,8 +6,8 @@ use rstest::rstest;
 #[rstest]
 #[case("2022-11-18 txn oops", 1)]
 #[case("\n\n2022-11-18 txn oops", 3)]
-fn error_on_first_directive(#[case] input: &str, #[case] expected_line: u64) {
-    let result = Parser::new(input).next();
-    let Some(Err(error)) = result else { panic!("Expected an error but was: {result:?}") };
+#[case("\n2022-11-17 txn\n2022-11-18 txn oops", 3)]
+fn simple_error(#[case] input: &str, #[case] expected_line: u64) {
+    let error = Parser::new(input).find_map(Result::err).unwrap();
     assert_eq!(error.line_number(), expected_line);
 }
