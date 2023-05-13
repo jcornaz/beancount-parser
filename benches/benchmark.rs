cfd9533c4f0147ad48264b0ed40640eb6f8173fc
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-05-13 10:22:24 +0200

    minor tweaks to the benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,4 +1,6 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use std::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 
 #[cfg(feature = "unstable")]
 use beancount_parser::parse;
@@ -7,14 +9,20 @@ use beancount_parser::Parser;
 const SAMPLE: &str = include_str!("../tests/samples/official.beancount");
 
 pub fn run_bench(c: &mut Criterion) {
-    c.bench_function("nom", |b| {
+    let mut group = c.benchmark_group("parse official example");
+    group.significance_level(0.01);
+    group.throughput(Throughput::Bytes(SAMPLE.len() as u64));
+    group.measurement_time(Duration::from_secs(10));
+    group.bench_function("nom implementation", |b| {
         b.iter(|| Parser::new(SAMPLE).collect::<Result<Vec<_>, _>>().unwrap())
     });
 
     #[cfg(feature = "unstable")]
-    c.bench_function("pest", |b| {
+    group.bench_function("pest implementation", |b| {
         b.iter(|| parse(SAMPLE).unwrap().collect::<Vec<_>>())
     });
+
+    group.finish();
 }
 
 criterion_group!(benches, run_bench);
