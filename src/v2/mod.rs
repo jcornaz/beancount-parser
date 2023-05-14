b1e4d2a0bd35c1757233f3099e0d9d5d2400bd7d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-05-14 22:21:53 +0200

    v2: parse date

--- src/v2/mod.rs
+++ src/v2/mod.rs
@@ -1,11 +1,46 @@
-pub fn parse(_input: &str) -> Result<Vec<Directive>, Error> {
-    Ok(Vec::new())
+mod date;
+
+pub use date::Date;
+use nom::{multi::many0, Finish};
+
+pub fn parse(input: &str) -> Result<BeancountFile, Error<'_>> {
+    match beancount_file(Span::new(input)).finish() {
+        Ok((_, content)) => Ok(content),
+        Err(nom::error::Error { input, .. }) => Err(Error(input)),
+    }
+}
+
+#[derive(Debug)]
+pub struct Error<'a>(Span<'a>);
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub struct BeancountFile {
+    pub directives: Vec<Directive>,
 }
 
 #[derive(Debug)]
 #[non_exhaustive]
-pub struct Error;
+pub struct Directive {
+    date: Date,
+}
 
-#[derive(Debug)]
-#[non_exhaustive]
-pub struct Directive;
+impl Directive {
+    #[must_use]
+    pub fn date(&self) -> Date {
+        self.date
+    }
+}
+
+type Span<'a> = nom_locate::LocatedSpan<&'a str>;
+type IResult<'a, O> = nom::IResult<Span<'a>, O>;
+
+fn beancount_file(input: Span<'_>) -> IResult<'_, BeancountFile> {
+    let (input, directives) = many0(directive)(input)?;
+    Ok((input, BeancountFile { directives }))
+}
+
+fn directive(input: Span<'_>) -> IResult<'_, Directive> {
+    let (input, date) = date::parse(input)?;
+    Ok((input, Directive { date }))
+}
--- tests/v2_spec.rs
+++ tests/v2_spec.rs
@@ -4,10 +4,53 @@ const COMMENTS: &str = include_str!("../tests/samples/comments.beancount");
 // TODO const SIMPLE: &str = include_str!("../tests/samples/simple.beancount");
 // TODO const OFFICIAL: &str = include_str!("../tests/samples/official.beancount");
 
-use beancount_parser::v2::parse;
+use beancount_parser::v2::{parse, Directive};
 use rstest::rstest;
 
 #[rstest]
-fn should_succeed_for_valid_input(#[values("", COMMENTS)] input: &str) {
+fn should_succeed_for_valid_input(#[values("", "\n", COMMENTS)] input: &str) {
     parse(input).expect("parsing should succeed");
+}
+
+#[rstest]
+#[case("2014-05-01 open Assets:Cash", 2014, 5, 1)]
+#[case("0001-01-01 open Assets:Cash", 1, 1, 1)]
+fn should_parse_date(
+    #[case] input: &str,
+    #[case] expected_year: u16,
+    #[case] expected_month: u8,
+    #[case] expected_day: u8,
+) {
+    let date = parse_single_directive(input).date();
+    assert_eq!(date.year, expected_year);
+    assert_eq!(date.month_of_year, expected_month);
+    assert_eq!(date.day_of_month, expected_day);
+}
+
+fn parse_single_directive(input: &str) -> Directive {
+    let directives = parse(input).expect("parsing should succeed").directives;
+    assert_eq!(
+        directives.len(),
+        1,
+        "unexepcted number of directives: {}",
+        directives.len()
+    );
+    directives.into_iter().next().unwrap()
+}
+
+#[rstest]
+fn should_reject_invalid_input(
+    #[values(
+        "14-05-01 open Assets:Cash",
+        "2014-5-01 open Assets:Cash",
+        "2014-05-1 open Assets:Cash",
+        "2014-00-01 open Assets:Cash",
+        "2014-13-01 open Assets:Cash",
+        "2014-05-00 open Assets:Cash",
+        "2014-05-32 open Assets:Cash"
+    )]
+    input: &str,
+) {
+    let result = parse(input);
+    assert!(result.is_err(), "{result:#?}");
 }
