fe90aefb188d94b48263cc1b495992fb30845767
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2024-03-02 23:06:31 +0100

    add `Date::new` and implement `FromStr` for `Date`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,7 +10,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ### Added
 
-* implement `FromStr` for `Account`
+* implement `FromStr` for `Account` and `Date`
+* `Date::new(year: u16, month: u8, day: u8) -> Self`
 
 
 ## [2.1.0] - 2024-01-29
--- src/date.rs
+++ src/date.rs
@@ -1,10 +1,11 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, str::FromStr};
 
 use nom::{
     bytes::complete::take,
     character::complete::{char, digit1},
-    combinator::{cut, map_res, peek, verify},
+    combinator::{all_consuming, cut, map_res, peek, verify},
     sequence::tuple,
+    Finish,
 };
 
 use super::{IResult, Span};
@@ -35,6 +36,41 @@ pub struct Date {
     pub month: u8,
     /// Day (of month)
     pub day: u8,
+}
+
+impl Date {
+    /// Create a new date from year, month and day
+    #[must_use]
+    pub fn new(year: u16, month: u8, day: u8) -> Self {
+        Self { year, month, day }
+    }
+}
+
+impl PartialOrd for Date {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Date {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.year
+            .cmp(&other.year)
+            .then_with(|| self.month.cmp(&other.month))
+            .then_with(|| self.day.cmp(&other.day))
+    }
+}
+
+impl FromStr for Date {
+    type Err = crate::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let span = Span::new(s);
+        match all_consuming(parse)(span).finish() {
+            Ok((_, date)) => Ok(date),
+            Err(_) => Err(crate::Error::new(s, span)),
+        }
+    }
 }
 
 pub(super) fn parse(input: Span<'_>) -> IResult<'_, Date> {
@@ -69,17 +105,21 @@ fn day(input: Span<'_>) -> IResult<'_, u8> {
     )(input)
 }
 
-impl PartialOrd for Date {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-impl Ord for Date {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.year
-            .cmp(&other.year)
-            .then_with(|| self.month.cmp(&other.month))
-            .then_with(|| self.day.cmp(&other.day))
+    #[test]
+    fn date_from_str_should_parse_valid_date() {
+        let date: Date = "2023-03-12".parse().unwrap();
+        assert_eq!(date.year, 2023);
+        assert_eq!(date.month, 3);
+        assert_eq!(date.day, 12);
+    }
+
+    #[test]
+    fn date_from_str_should_not_parse_invalid_date() {
+        let result: Result<Date, _> = "2023-03-12oops".parse();
+        assert!(result.is_err(), "{result:?}");
     }
 }
