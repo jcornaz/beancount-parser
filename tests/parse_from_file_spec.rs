1f23ea580d52c852578609699e2668bef7867e10
Jakob Hellermann <hellermann@sipgate.de>
2026-01-15 10:21:02 +0100

    add iterator based parser method

--- tests/parse_from_file_spec.rs
+++ tests/parse_from_file_spec.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 
 use rstest::rstest;
 
-use beancount_parser::BeancountFile;
+use beancount_parser::{BeancountFile, Entry};
 
 #[rstest]
 #[case("comments.beancount", 0)]
@@ -17,4 +17,18 @@ fn can_parse_example_files(#[case] file_name: &str, #[case] expected_directive_c
     let mut file = BeancountFile::<f64>::default();
     beancount_parser::read_files([path], |entry| file.extend(Some(entry))).unwrap();
     assert_eq!(file.directives.len(), expected_directive_count);
+}
+
+#[rstest]
+#[case("comments.beancount", 0)]
+#[case("simple.beancount", 16)]
+#[case("official.beancount", 1714 + 2)] // include entries are emitted as well
+#[case("includes.beancount", 1731 + 2)]
+fn can_parse_example_files_iter(#[case] file_name: &str, #[case] expected_directive_count: usize) {
+    let mut path: PathBuf = "./tests/samples".into();
+    path.push(file_name);
+    let directives = beancount_parser::read_files_iter([path])
+        .collect::<Result<Vec<Entry<f64>>, _>>()
+        .unwrap();
+    assert_eq!(directives.len(), expected_directive_count);
 }
